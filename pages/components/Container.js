01d41932f47cbe7b5a384b1a3905bc028ed353c2
AsadullahSamo <asadsamoon8@gmail.com>
2024-05-17 22:26:01 +0500

    add key prop to fix production issue

--- pages/components/Container.js
+++ pages/components/Container.js
@@ -1,20 +1,20 @@
 import React from 'react'
 
 export default function Container() {
-
-	const numberOfColumns = 15
   
   return (
     
     
       <table className='w-[60%] h-[500px] bg-white'>
-					{Array.from({ length: numberOfColumns }).map((_, index) => (
-						<tr className='border-[1px] border-black'>
-							{Array.from({ length: numberOfColumns }).map((_, index) => (
+				<thead>
+					{Array.from({ length: 15 }).map((_, index) => (
+						<tr key={index} className='border-[1px] border-black'>
+							{Array.from({ length: 15 }).map((_, index) => (
 								<td key={index} className='border-[1px] border-black'> </td>
 							))}
 						</tr>
 					))}
+				</thead>
     	</table>
 
   )
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Container from "./components/Container";
+import Head from "next/head";
 
 export default function Home() {
   
   return (
     <main className="min-h-screen w-[100%] bg-[#cac7c8] flex mx-auto flex-col items-center justify-center"> 
+      <Head>
+        <title> Level Editor </title>
+        <link rel="icon" href="/public/assets/favicon/favicon.png" />
+      </Head>
+
       <Container />
     </main>
   );
