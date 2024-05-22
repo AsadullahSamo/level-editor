9d2d3f1657eed5bea9bae6812faff6efedac4e61
AsadullahSamo <asadsamoon8@gmail.com>
2024-05-22 23:25:23 +0500

    redefine undo and erase functionality

--- pages/components/Container.js
+++ pages/components/Container.js
@@ -8,6 +8,7 @@ import Dialog from './Dialog';
 export default function Container() {
 
 	const [tableData, setTableData] = useState(Array.from({ length: 25 }, () => Array.from({ length: 25 }, () => '')));
+	const [assetAddedIndex, setAssetAddedIndex] = useState([]);
 	const [cursorIcon, setCursorIcon] = useState('/assets/tiles/svg/tile-10.svg');
 	const [zoomValue, setZoomValue] = useState(1);
 	const [xCoord, setXCoord] = useState(0);
@@ -19,29 +20,29 @@ export default function Container() {
 	const [images, setImages] = useState([])
 
 	const handleClick = (index) => {
-		setActive('');
+		setActive('draw');
 		setCurrentAsset(`/assets/tiles/svg/tile-${index}.svg`);
 		setCursorIcon(`/assets/tiles/svg/tile-${index}.svg`);
 		setCursorIndex(index)
 	};
 	
-	const handleMouseDown = (rowIndex, colIndex) => {
-		setIsDrawing(true);
-		if (active === 'erase') {
-			eraseAsset(rowIndex, colIndex);
-		} else {
-			drawImage(rowIndex, colIndex);
-		}
-	};
+	// const handleMouseDown = (rowIndex, colIndex) => {
+	// 	setIsDrawing(true);
+	// 	if (active === 'erase') {
+	// 		eraseAsset(rowIndex, colIndex);
+	// 	} else {
+	// 		drawImage(rowIndex, colIndex);
+	// 	}
+	// };
 
-    const handleMouseMove = (rowIndex, colIndex) => {
-			if (!isDrawing) return;
-			if (active === 'erase') {
-				eraseAsset(rowIndex, colIndex);
-			} else {
-				drawImage(rowIndex, colIndex);
-			}
-    };
+    // const handleMouseMove = (rowIndex, colIndex) => {
+		// 	if (!isDrawing) return;
+		// 	if (active === 'erase') {
+		// 		eraseAsset(rowIndex, colIndex);
+		// 	} else {
+		// 		drawImage(rowIndex, colIndex);
+		// 	}
+    // };
 
     const handleMouseUp = () => {
         setIsDrawing(false);
@@ -49,17 +50,24 @@ export default function Container() {
 
 	const undoMove = () => {
 		setActive('undo');
+		const newTableData = [...tableData];
+		const lastAsset = assetAddedIndex.pop();
+		if (lastAsset) {
+			newTableData[lastAsset.rowIndex][lastAsset.colIndex] = '';
+		}
+		setTableData(newTableData);
 	};
 
 	const handleUndoKeyDown = (e) => {
 		if (e.key === 'z' && e.ctrlKey) {
+			e.preventDefault();
 			undoMove();
 		}
 	};
 	
 	const getCurrentAsset = () => {
 		setCursorIcon(currentAsset);
-		setActive('')
+		setActive('draw')
 	};
 
 	const erase = () => {
@@ -97,25 +105,37 @@ export default function Container() {
 			newTableData[rowIndex][colIndex] = '';
 		}
 		setTableData(newTableData);
+		const newAssetAddedIndex = [...assetAddedIndex];
+		// newAssetAddedIndex.forEach((asset, index) => {
+		// 	if (asset.rowIndex === rowIndex && asset.colIndex === colIndex) {
+		// 		newAssetAddedIndex.splice(index, 1);
+		// 	}
+		// });
+		newAssetAddedIndex.splice(newAssetAddedIndex.findIndex(asset => asset.rowIndex === rowIndex && asset.colIndex === colIndex), 1);
+		setAssetAddedIndex(newAssetAddedIndex);
 	};
 
 	const handleCellClick = (rowIndex, colIndex) => {
     if (active === 'erase') {
 				eraseAsset(rowIndex, colIndex);
-    } else {
-        drawImage(rowIndex, colIndex);
-    }
+    } else if(active === 'draw') {
+			drawImage(rowIndex, colIndex);
+		}
 	};
 	
 	const drawImage = (rowIndex, colIndex) => {
-		// console.log(rowIndex, colIndex);
     setActive('draw');
     const newTableData = [...tableData];
     if(rowIndex !== undefined && colIndex !== undefined) {
 			tableData[rowIndex][colIndex] === '' ? tableData[rowIndex][colIndex] = [currentAsset] : tableData[rowIndex][colIndex].push(currentAsset);
 		}
     setTableData(newTableData);
+		setAssetAddedIndex([...assetAddedIndex, {rowIndex, colIndex}]);
 	};
+
+	useEffect(() => {
+		console.log(assetAddedIndex)
+	}, [assetAddedIndex])
 	
 	
 	useEffect(() => {
@@ -133,7 +153,7 @@ export default function Container() {
 		return () => {
 			window.removeEventListener('keydown', handleUndoKeyDown);
 		};
-	}, [images]);
+	}, [tableData]);
 
 
   return (
@@ -150,12 +170,12 @@ export default function Container() {
 
 		{/* Features */}
 		<div className='w-[100%] h-[40px] bg-[#393939] fixed top-[0%] border-[1px] border-[#7a7a7a]' style={{zIndex: 10}}>
-			<ul className='flex gap-5 justify-start pl-3 w-[15%] border-l-[1px] border-[#7a7a7a] h-[37px] bg-[#212121] absolute left-[6.4rem]'> 
-					<Image onClick={getCurrentAsset} src={currentAsset} className={`hover:cursor-pointer`} width={24} height={24} style={{objectFit: 'contain'}}/>
+			<ul className='flex gap-5 justify-start pl-3 w-[16%] border-l-[1px] border-[#7a7a7a] h-[37px] bg-[#212121] absolute left-[6.4rem]'> 
+					<Image onClick={getCurrentAsset} src={currentAsset} className={`${active === 'draw' ? 'bg-blue-500' : ''} px-2 w-10 hover:cursor-pointer`} width={24} height={24} style={{objectFit: 'contain'}}/>
 					<Image onClick={undoMove} title='undo (Ctrl + Z)' popovertarget="mypopover" src={`/assets/icons/undo-icon.svg`} className={`hover:cursor-pointer px-2 ${active === "undo" ? 'bg-blue-500' : ''} `} width={42} height={42} style={{objectFit: 'contain'}}/>
 					<Image onClick={erase} title='erase' src={`/assets/icons/rubber-icon.svg`} className={`hover:cursor-pointer px-2 w-10 -mx-2 ${active === "erase" ? 'bg-blue-500' : ''} `} width={67} height={67} style={{objectFit: 'contain'}}/>
 			</ul>
-			<ul className='flex gap-5 justify-center w-[7%] border-l-[1px] border-r-[1px] border-[#7a7a7a] h-[37px] bg-[#212121] absolute left-64'> 
+			<ul className='flex gap-5 justify-center w-[7%] border-l-[1px] border-r-[1px] border-[#7a7a7a] h-[37px] bg-[#212121] absolute left-72'> 
 				<Image onClick={zoomIn} src={`/assets/icons/zoom-in-icon.svg`} className={`hover:cursor-pointer px-2 ${active === "zoomIn" ? 'bg-blue-500' : ''} `} width={42} height={42} style={{objectFit: 'contain'}}/>
 				<Image onClick={zoomOut} src={`/assets/icons/zoom-out-icon.svg`} className={`hover:cursor-pointer px-2 ${active === "zoomOut" ? 'bg-blue-500' : ''} `} width={42} height={42} style={{objectFit: 'contain'}}/>
 			</ul>	
@@ -172,8 +192,8 @@ export default function Container() {
 				{Array.from({ length: 25 }).map((_, colIndex) => (
 					<td key={colIndex} className='relative p-0 m-0 border-collapse whitespace-nowrap' style={{ cursor: `url(${cursorIcon}), auto`, width: '64px', height: '64px', border: '1px solid black', padding: 0, margin: 0 }}
 						onClick={() => handleCellClick(rowIndex, colIndex)}
-						onMouseMove={() => handleMouseMove(rowIndex, colIndex)}
-						onMouseDown={() => handleMouseDown(rowIndex, colIndex)}
+						// onMouseMove={() => handleMouseMove(rowIndex, colIndex)}
+						// onMouseDown={() => handleMouseDown(rowIndex, colIndex)}
 					>
 					 {Array.isArray(tableData[rowIndex]?.[colIndex]) &&
                 tableData[rowIndex][colIndex].map((image, index) => (
